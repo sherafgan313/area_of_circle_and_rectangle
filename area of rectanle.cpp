8eef46b16652e6d0e9258025368d0455dba93fd7
Sher Afgan <145536822+sherafgan313@users.noreply.github.com>
2024-06-23 00:16:59 -0700

    area of rectangle

---
+++ area of rectanle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	int length,width,area;
+	
+	cout<<"Enter Length : ";
+	cin>>length;
+	
+	cout<<"Enter Width : ";
+	cin>>width;
+	
+	area=length*width;
+	
+	cout<<"Area of Rectangle is = "<<area<<endl;
+	return 0;
+}
